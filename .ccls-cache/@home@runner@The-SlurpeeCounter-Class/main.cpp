ea17bc2a42cbc57dd428f00ae9171c35e09611fc
zwhippo9 <zwhippo9@gmail.com>
2024-10-23 21:58:44 +0000

    Header File

--- .ccls-cache/@home@runner@The-SlurpeeCounter-Class/main.cpp
+++ .ccls-cache/@home@runner@The-SlurpeeCounter-Class/main.cpp
@@ -1,3 +1,29 @@
-#include <iostream>
+#ifndef SLURPEECOUNTER_H
+#define SLURPEECOUNTER_H
 
-int main() { std::cout << "Hello World!\n"; }
+class SlurpeeCounter {
+private:
+  int counter;
+
+public:
+  //initialize counter to 0
+  SlurpeeCounter();
+
+  //set to non negative value
+  void setCounter(int value);
+
+  //retrieve value
+  void getCounter() const;
+
+  //increment counter by 1
+  void incrementCounter();
+
+  //decrement counter by 1
+  void decrementCounter();
+
+  //reset counter to 0
+  void resetCounter();
+
+};
+
+#endif
